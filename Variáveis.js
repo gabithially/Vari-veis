6b39f10091429e865c315229968d9b2775759339
gabithially <gabrielathially@yahoo.com>
2023-07-04 10:56:52 -0300

    Add commits about the code

--- Variáveis.js
+++ Variáveis.js
@@ -4,8 +4,8 @@ const dataNascimento = "02/04/1981";
 let idade = "23"; 
 let endereço = "Paulista";
 
-console.log(`Primeiro nome:   ${primeiroNome}`); 
-console.log(`Sobrenome: ${segundoNome}`);
-console.log(`Data de nascimento: ${dataNascimento}`);
-console.log(`Idade: ${idade}`);
-console.log(`Endereço: ${endereço}`); 
+console.log(`Primeiro nome:   ${primeiroNome}`); // EXIBE O NOME DO USUÁRIO
+console.log(`Sobrenome: ${segundoNome}`); // EXIBE O SOBRENOME DO USUÁRIO
+console.log(`Data de nascimento: ${dataNascimento}`); // EXIBE A DATA DE NASCIMENTO DO USUÁRIO
+console.log(`Idade: ${idade}`); // EXIBE A IDADE DO USUÁRIO
+console.log(`Endereço: ${endereço}`); // EXIBE O ENDEREÇO DO USUÁRIO
