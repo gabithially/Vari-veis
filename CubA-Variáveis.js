916bf7e00dca346c660504d2b67fbde373ba2510
gabithially <gabrielathially@yahoo.com>
2023-07-04 13:27:52 -0300

    activities corrections

--- CubA-Variáveis.js
+++ CubA-Variáveis.js
@@ -6,6 +6,6 @@ let numeroPopulacaoPaulista = 300.000;
 
 console.log(`Número da População de Paulista-PE: ${numeroPopulacaoPaulista}`);
 
-let numeroPopulacaoAtualizado = 334.376;
+numeroPopulacaoPaulista = 334.376;
 
-console.log(`Número Atualizado da População de Paulista-PE: ${numeroPopulacaoAtualizado}`);
+console.log(`Número da População de Paulista-PE: ${numeroPopulacaoPaulista}`);
