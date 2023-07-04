6744d47595bfa62d2990fd2aaff692f96e1e585d
gabithially <gabrielathially@yahoo.com>
2023-07-04 11:05:03 -0300

    test variable number

--- Variáveis.js
+++ Variáveis.js
@@ -1,7 +1,7 @@
 const primeiroNome = "João";
 const segundoNome = "Victor";
 const dataNascimento = "02/04/1981";
-let idade = "23"; 
+let idade = 23; 
 let endereço = "Paulista";
 
 console.log(`Primeiro nome:   ${primeiroNome}`); // EXIBE O NOME DO USUÁRIO
