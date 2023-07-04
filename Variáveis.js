49c5fbfed3f6db6266b1ee5faae475364235e798
gabithially <gabrielathially@yahoo.com>
2023-07-04 10:52:24 -0300

    to test variables

---
+++ Variáveis.js
@@ -0,0 +1,11 @@
+const primeiroNome = "João";
+const segundoNome = "Victor";
+const dataNascimento = "02/04/1981";
+let idade = "23"; 
+let endereço = "Paulista";
+
+console.log(`Primeiro nome:   ${primeiroNome}`); 
+console.log(`Sobrenome: ${segundoNome}`);
+console.log(`Data de nascimento: ${dataNascimento}`);
+console.log(`Idade: ${idade}`);
+console.log(`Endereço: ${endereço}`); 
