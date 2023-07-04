5e9c8ecc9bf1eaade7309c94949bd4bfb90e6a48
gabithially <gabrielathially@yahoo.com>
2023-07-04 13:22:55 -0300

    Activities about variables

---
+++ CubA-Variáveis.js
@@ -0,0 +1,11 @@
+const cidadeNatal = "Paulista";
+
+console.log(`Cidade Natal: ${cidadeNatal}`);
+
+let numeroPopulacaoPaulista = 300.000;
+
+console.log(`Número da População de Paulista-PE: ${numeroPopulacaoPaulista}`);
+
+let numeroPopulacaoAtualizado = 334.376;
+
+console.log(`Número Atualizado da População de Paulista-PE: ${numeroPopulacaoAtualizado}`);
