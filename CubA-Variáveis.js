8b80cf1dcb2e5ba0d3e92e6c51c95ab08e194ceb
gabithially <gabrielathially@yahoo.com>
2023-07-04 13:56:45 -0300

    add commits to the exercicies

--- CubA-Variáveis.js
+++ CubA-Variáveis.js
@@ -1,6 +1,10 @@
+// EXERCÍCIO 1
+
 const cidadeNatal = "Paulista";
 
 console.log(`Cidade Natal: ${cidadeNatal}`);
+
+// EXERCÍCIO 2
 
 let numeroPopulacaoPaulista = 300.000;
 
@@ -9,3 +13,6 @@ console.log(`Número da População de Paulista-PE: ${numeroPopulacaoPaulista}`)
 numeroPopulacaoPaulista = 334.376;
 
 console.log(`Número da População de Paulista-PE: ${numeroPopulacaoPaulista}`);
+
+// EXERCÍCIO 3
+
