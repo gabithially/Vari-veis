7993ab43fdd14ff628938ae15fa9cda2a4223c8d
gabithially <gabrielathially@yahoo.com>
2023-07-04 14:35:18 -0300

    Criated 'concatenar' to do the exercicies

---
+++ Concatenar.js
@@ -0,0 +1,15 @@
+let x = 10;
+let y = 2;
+let resultado 
+
+resultado = x + y;
+    console.log(`A soma de ${x} somado por ${y} é igual a ${resultado}.`)
+
+resultado = x - y;
+    console.log(`A subtração de ${x} subtraído por ${y} é igual a ${resultado}.`)
+
+resultado = x * y;
+    console.log(`A multiplicação de ${x} multiplicado por ${y} é igual a ${resultado}.`)
+
+resultado = x / y;
+    console.log(`A divisão de ${x} dividido por ${y} é igual a ${resultado}.`)
